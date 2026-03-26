b87ca5516a0a6f050e33eb824d7511775b357ba5
CYRILLE BOUCHER <15726172+BouCyr@users.noreply.github.com>
2026-03-26 23:18:25 +0100

    Create TestqApplication class

---
+++ src/main/java/com/example/testq/TestqApplication.java
@@ -0,0 +1,12 @@
+package com.example.testq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestqApplication.class, args);
+    }
+}
